15a92922d631a6e9219c8bc8fab03a0e9622554f
Tomas Duchek <neb7pxuk@duchic.anonaddy.com>
2024-09-13 23:31:31 +0200

    Fix function Red, other function delete (will be rewrite), fixed js load

--- js/script.js
+++ js/script.js
@@ -32,131 +32,16 @@ function Hello() {
     alert("Hello, World");
  }
 
-function Red(player) {
-    if(player === "p1"){
-        console.log("x");
-        p2break = 0;
-        p1point = p1point+1;
-        p1break = p1break+1;
-        p1redused = p1redused+1;
-        document.getElementById("p1points").innerHTML = "p1point";
-        document.getElementById("p1break").innerHTML = "p1break";
+function Red(p1) {
+    if (p1 === "p1red"){
+        console.log("p1");
     }
-
-    if(player == "p2"){
-        p1break = 0;
-        p2point = p2point+1;
-        p2break = p2break+1;
-        p2redused = p2redused+1;
-    }
-}
-
-function Black(player) {
-    if(player == "p1"){
-        p1point = p1point+7;
-        p1break = p1break+7;
-        p1blackused = p1blackused+1;
-        //body co zbývají na stole
-        //rozdíl mezi hráči
-    }
-
-    if(player == "p2"){
-        p2point = p2point+7;
-        p2break = p2break+7;
-        p2blackused = p2blackused+1;
-        //body co zbývají na stole
-        //rozdíl mezi hráči
-    }
-}
-
-function Pink(player) {
-    if(player == "p1"){
-        p1point = p1point+6;
-        p1break = p1break+6;
-        p1pinkused = p1pinkused+1;
-        //body co zbývají na stole
-        //rozdíl mezi hráči
-    }
-
-    if(player == "p2"){
-        p2point = p2point+6;
-        p2break = p2break+6;
-        p2pinkused = p2pinkused+1;
-        //body co zbývají na stole
-        //rozdíl mezi hráči
-    }
-}
-
-function Blue(player) {
-    if(player == "p1"){
-        p1point = p1point+5;
-        p1break = p1break+5;
-        p1blueused = p1blueused+1;
-        //body co zbývají na stole
-        //rozdíl mezi hráči
-    }
-
-    if(player == "p2"){
-        p2point = p2point+5;
-        p2break = p2break+5;
-        p2blueused = p2blueused+1;
-        //body co zbývají na stole
-        //rozdíl mezi hráči
-    }
-}
-
-function Brown(player) {
-    if(player == "p1"){
-        p1point = p1point+4;
-        p1break = p1break+4;
-        p1brownused = p1brownused+1;
-        //body co zbývají na stole
-        //rozdíl mezi hráči
-    }
-
-    if(player == "p2"){
-        p2point = p2point+4;
-        p2break = p2break+4;
-        p2brownused = p2brownused+1;
-        //body co zbývají na stole
-        //rozdíl mezi hráči
-    }
-}
-
-function Green(player) {
-    if(player == "p1"){
-        p1point = p1point+3;
-        p1break = p1break+3;
-        p1greenused = p1greenused+1;
-        //body co zbývají na stole
-        //rozdíl mezi hráči
-    }
-
-    if(player == "p2"){
-        p2point = p2point+3;
-        p2break = p2break+3;
-        p2greenused = p2greenused+1;
-        //body co zbývají na stole
-        //rozdíl mezi hráči
-    }
-}
-
-function Yellow(player) {
-    if(player == "p1"){
-        p1point = p1point+2;
-        p1break = p1break+2;
-        p1yellowused = p1yellowused+1;
-        //body co zbývají na stole
-        //rozdíl mezi hráči
-    }
-
-    if(player == "p2"){
-        p2point = p2point+2;
-        p2break = p2break+2;
-        p2yellowused = p2yellowused+1;
-        //body co zbývají na stole
-        //rozdíl mezi hráči
-    }
+    p2break = 0;
+    p1point = p1point+1;
+    p1break = p1break+1;
+    p1redused = p1redused+1;
+    document.getElementById("p1points").innerHTML = p1point;
+    document.getElementById("p1break").innerHTML = p1break;
 }
 
 function RedCount() {
